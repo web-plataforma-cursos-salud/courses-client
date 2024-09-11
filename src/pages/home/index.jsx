dde55c26176791359b2931d6bc3ddebcf0cbca38
alexisfrene <alexisfrene@gmail.com>
2024-09-11 14:30:36 -0300

    initial development branch

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import Button from "@mui/material/Button";
 
 export default function Home() {
-  return <Button variant="contained">Hello world</Button>;
+  return <Button variant="contained">Hello worldd</Button>;
 }
